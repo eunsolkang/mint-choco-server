ace87b2d50cb089030d01410fb555d2a829abaa1
강은솔 <eunsol2953@naver.com>
2017-07-30 20:56:07 +0900

    first commit

---
+++ RoomManager.js
@@ -0,0 +1,70 @@
+
+
+function RoomManager(io){
+
+  var RmMg = this;
+  RmMg.userCnt = 0;
+  RmMg.rooms = [];
+  RmMg.roomIndex = [];
+  RmMg.create = function(data0,data1){
+    var roomNum = data0.socket.id+data1.socket.id;
+    var room = new Room(roomNum,data0,data1);
+    data0.socket.join(roomNum);
+    data1.socket.join(roomNum);
+    io.to(roomNum).emit("pick", {a : "a"});
+    RmMg.rooms[roomNum] = room;
+    RmMg.roomIndex[data0.socket.id] = roomNum;
+    RmMg.roomIndex[data1.socket.id] = roomNum;
+
+    console.log("Room Created :", roomNum);
+    // data0.socket.on('userData', function(data){
+    //   io.to(roomNum).emit('userData', data);
+    // });
+    // data1.socket.on('userData', function(data){
+    //   io.to(roomNum).emit('userData', data);
+    // });
+    // data0.socket.on('gameLoad', function(){
+    //   io.to(roomNum).emit('gameLoad', {ready : ++userCnt});
+    // });
+    // data1.socket.on('gameLoad', function(){
+    //   io.to(roomNum).emit('gameLoad', {ready : ++userCnt});
+    // });
+    // data0.socket.on('move', function(data){
+    //   socket.broadcate.to(roomNum).emit('move', data);
+    // });
+    // data1.socket.on('move', function(data){
+    //   socket.broadcate.to(roomNum).emit('move', data);
+    // });
+    // data0.socket.on('bullet', function(data){
+    //   io.to(roomNum).emit('bullet', {a : "a"});
+    // });
+    // data1.socket.on('bullet', function(data){
+    //   io.to(roomNum).emit('bullet', {a : "a"});
+    // });
+    // data0.socket.on('rotate', function(data){
+    //   data0.socket.broadcate.to(roomNum).emit('rotate', data);
+    // });
+    // data1.socket.on('rotate', function(data){
+    //   data0.socket.broadcate.to(roomNum).emit('rotate', data);
+    // })
+  };
+  RmMg.destroy = function(roomNum, LbMg){
+      var room = RmMg.rooms[roomNum];
+
+      room.players.forEach(function(data){
+        LbMg.push(data);
+        delete RmMg.roomIndex[data.socket.id];
+      });
+      console.log(room);
+      delete RmMg.rooms[roomNum];
+
+  };
+}
+
+function Room(num, player0, player1)
+{
+  this.num = num;
+  this.players = [player0, player1];
+}
+
+module.exports = RoomManager;
---
+++ app.js
@@ -0,0 +1,74 @@
+
+// var express = require('express');
+// var app     = express();
+// var http    = require('http').Server(app);
+// var io = require('socket.io')(http);
+// var path    = require('path');
+// http.listen(7727, function(){
+//   console.log("server on!: http://localhost:3000/");
+// });
+// app.use(express.static(path.join(__dirname,"public")));
+
+var io = require('socket.io')({
+	transports: ['websocket'],
+});
+io.attach(3000);
+var userList = [];
+
+
+
+// var socket = io();
+var lobbyManager = new (require('./LobbyManager.js'))(io);
+var roomManager = new (require('./RoomManager.js'))(io);
+
+io.on('connection', function(socket){
+	console.log('영남이형');
+	var nickname;
+	socket.on('join', function(data){
+		nickname = data;
+		console.log(data);
+		userList.push(nickname);
+		socket.emit('join', nickname);
+		console.log('userList : ', userList);
+	});
+
+	socket.on('pick', function(data){
+		var tmp = data.pick;
+		console.log(tmp);
+		lobbyManager.push({socket, nickname, tmp});
+		lobbyManager.dispatch(roomManager);
+	});
+
+	socket.on('userData', function(data){
+		var roomNum = roomManager.roomIndex[socket.id];
+		io.to(roomNum).emit('userData', data);
+	});
+	socket.on('gameLoad', function(){
+		var roomNum = roomManager.roomIndex[socket.id];
+		io.to(roomNum).emit('gameLoad', {ready : ++roomManager.userCnt});
+	});
+	socket.on('move', function(data){
+		var roomNum = roomManager.roomIndex[socket.id];
+		socket.broadcast.to(roomNum).emit('move', data);
+	});
+	socket.on('bullet', function(data){
+		var roomNum = roomManager.roomIndex[socket.id];
+		io.to(roomNum).emit('bullet', {a : "a"});
+	});
+	socket.on('rotate', function(data){
+		var roomNum = roomManager.roomIndex[socket.id];
+		socket.broadcast.to(roomNum).emit('rotate', data);
+	});
+
+	socket.on('disconnect', function(){;
+		var roomNum = roomManager.roomIndex[socket.id];
+    if(roomNum){
+      roomManager.destroy(roomNum, lobbyManager);
+    }
+		roomManager.userCnt --;
+    lobbyManager.kick(socket);
+		lobbyManager.dispatch(roomManager);
+    console.log('user disconnected: ', socket.id);
+    //console.log(socket);
+  });
+});
